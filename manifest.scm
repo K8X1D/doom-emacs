b76a46307074e0707ab428a11255cc6d899082e3
K8X1D <k8x1d@protonmail.ch>
2023-01-22 13:08:11 -0500

    v1 guix replicable set-up

---
+++ manifest.scm
@@ -0,0 +1,9 @@
+(specifications->manifest
+ (list
+  ;; ;; Doom emacs ;;
+  "emacs-pgtk-native-comp" ;; flatwhatson version
+  ;;"k8x1d-emacs-pgtk" ;; k8x1d version
+  "git"
+  "ripgrep"
+  "fd"
+  ))
